45277218cb4fe78c5331e2e86fe7d4ea7f092fa8
Alberto <edhoru@hotmail.com>
2019-05-20 20:23:45 -0500

    Initial Commit

---
+++ StreamTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StreamTest
+//
+//  Created by Alberto Huerdo on 5/20/19.
+//  Copyright © 2019 Huerdo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
